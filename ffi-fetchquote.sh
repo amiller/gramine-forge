c39420023f095ae315e5730211843c7639e2c8a4
Andrew Miller <socrates1024@gmail.com>
2023-11-07 22:34:18 -0600

    add ffi

---
+++ ffi-fetchquote.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -s http://dummyattest.ln.soc1024.com/$1
--- lib/AttestForge.sol
+++ lib/AttestForge.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.8;
-
-import "./Suave.sol";
+import "github.com/Arachnid/solidity-stringutils/strings.sol";
 
 interface Vm {
     function ffi(string[] calldata commandInput) external view returns (bytes memory result);
@@ -10,29 +9,19 @@ interface Vm {
 library AttestForge {
     Vm constant vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
-    function forgeIt(string memory addr, bytes memory data) internal view returns (bytes memory) {
-        string memory dataHex = iToHex(data);
+    function attest() view returns (bytes memory) {
+	forgeIt(msg.caller, bytes());
+    }
 
-        string[] memory inputs = new string[](4);
-        inputs[0] = "docker";
-        inputs[1] = "forge";
-        inputs[2] = addr;
-        inputs[3] = dataHex;
+    function forgeIt(string memory addr, bytes memory data) internal view returns (bytes memory) {
+        string memory dataHex = "9113b0be77ed5d0d68680ec77206b8d587ed40679b71321ccdd5405e4d54a6820000000000000000000000000000000000000000000000000000000000000000"
+
+        string[] memory inputs = new string[](3);
+        inputs[0] = "bash";
+        inputs[1] = "ffi-fetchquote.sh";
+        inputs[2] = dataHex;
 
         bytes memory res = vm.ffi(inputs);
         return res;
-    }
-
-    function iToHex(bytes memory buffer) public pure returns (string memory) {
-        bytes memory converted = new bytes(buffer.length * 2);
-
-        bytes memory _base = "0123456789abcdef";
-
-        for (uint256 i = 0; i < buffer.length; i++) {
-            converted[i * 2] = _base[uint8(buffer[i]) / _base.length];
-            converted[i * 2 + 1] = _base[uint8(buffer[i]) % _base.length];
-        }
-
-        return string(abi.encodePacked("0x", converted));
     }
 }
